23c7ac4813abab844be705772cb24de3775ead32
HOTKURWA <ddduuuxxxaa@mail.ru>
2018-05-06 18:20:57 +0600

    tune DataBase
    tune DataBase

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -80,6 +80,7 @@ class BasePanel extends JPanel
     {        
         SomeButton();
         System.out.println("Hi Developer");
+        testDatabase();
     }
     private void Customer() 
     {
@@ -229,29 +230,28 @@ class BasePanel extends JPanel
                 }
             }
        });
-
-//        private void testDatabase() {
-//        try {
-//            Class.forName("org.postgresql.Driver");
-//            String url = "jdbc:postgresql://localhost:5432/contactdb";
-//            String login = "postgres";
-//            String password = "postgres";
-//            Connection con = DriverManager.getConnection(url, login, password);
-//            try {
-//                Statement stmt = con.createStatement();
-//                ResultSet rs = stmt.executeQuery("SELECT * FROM JC_CONTACT");
-//                while (rs.next()) {
-//                    String str = rs.getString("contact_id") + ":" + rs.getString(2);
-//                    System.out.println("Contact:" + str);
-//                }
-//                rs.close();
-//                stmt.close();
-//            } finally {
-//                con.close();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        }
     }
+
+    private void testDatabase() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/DataBase";
+            String login = "postgres";
+            String password = "14589";
+            Connection con = DriverManager.getConnection(url, login, password);
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM \"public\".\"Customer\"");
+                while (rs.next()) {
+                    String str = rs.getString("Логин заказчика") + ":" + rs.getString("Пароль заказчика");
+                    System.out.println(" " + str);
+                }
+                rs.close();
+                stmt.close();
+            } finally {
+                con.close();
+            }
+        } catch (Exception e) 
+            {e.printStackTrace();}
+        }
 }
